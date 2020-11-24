12c6a4c9a4d09a3ffeeb130eb64e76d2606c2ec3
sidahmed <ahorob57@gmail.com>
2020-11-24 15:56:53 +0100

    ui class addbook dipslay book

--- app.js
+++ app.js
@@ -1,17 +1,65 @@
 // Book Class: represent a book
 
+class Book {
+    constructor(title, author, isbn) {
+        this.title = title
+        this.author = author
+        this.isbn = isbn
+    }
+}
 
 // UI Class: hanles UI tasks
 
+class UI {
+    static displayBooks() {
+        const storedBooks = [
+            {
+                title: "book one",
+                author: "jhon doe",
+                isbn: "7353"
+            }
+        ]
+
+        const books = storedBooks
+
+        books.forEach(book => UI.addBookToList(book))
+    }
+
+    static addBookToList(book) {
+        const list = document.getElementById('book-list')
+        
+        const row = document.createElement('tr')
+
+        row.innerHTML = `
+          <td>${book.title}</td>
+          <td>${book.author}</td>
+          <td>${book.isbn}</td>
+          <td><a href="#" class="btn btn-danger btn-sm delete">del</a></td>
+        `
+    }
+}
 
 // Store Class: handles storage
 
+class Storage {
+    constructor(bookForm) {
+        this.bookForm = document.getElementById(bookForm)
+    }
+    
+    storeBooks(book) {
+        
+
+    }
+}
 
 // Event: display Books
 
 
 // Event: Add a Book
 
+class AddBook {
+    constructor(event, book) {}
+}
 
 // Event: remove a book
 
