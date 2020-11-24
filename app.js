21699525529679a0361f2f2949e42673d64b586e
sidahmed <ahorob57@gmail.com>
2020-11-24 15:22:07 +0100

    comment added

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-// start coding..
+// Book Class: represent a book
+
+
+// UI Class: hanles UI tasks
+
+
+// Store Class: handles storage
+
+
+// Event: display Books
+
+
+// Event: Add a Book
+
+
+// Event: remove a book
+
