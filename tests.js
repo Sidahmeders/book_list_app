80abdb30115e4dcd871d7b81ec4e832f889f7a5d
sidahmed <ahorob57@gmail.com>
2020-12-06 14:19:01 +0100

    add test

---
+++ tests.js
@@ -0,0 +1,15 @@
+// write your leetcode tests here
+/*
+  merge two sorted list
+  Input: l1 = [1,2,4], l2 = [1,3,4]
+  Output: [1,1,2,3,4,4]
+*/
+
+// your pseudo-code goes here
+/*
+*/
+
+
+
+
+console.log(maxArea([1,8,6,2,5,4,8,3,7]))
