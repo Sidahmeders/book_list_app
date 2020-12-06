0583d9246b152269b09c124fed5e914899f6bfaf
sidahmed <ahorob57@gmail.com>
2020-12-06 14:20:16 +0100

    add leetcodes merge0twolists

--- tests.js
+++ tests.js
@@ -11,5 +11,26 @@
 
 
 
+var mergeTwoLists = function(l1, l2) {
+  let dummy = new ListNode()
+  let head = dummy
+
+  while (l1 && l2) {
+    if (l1.val < l2.val) {
+      dummy.next = l1
+      l1 = l1.next
+    } else {
+      dummy.next = l2
+      l2 = l2.next
+    }
+    dummy = dummy.next
+  }
+
+  if (l1) dummy.next = l1
+  if (l2) dummy.next = l2
+  
+  return head.next
+}
+
 
 console.log(maxArea([1,8,6,2,5,4,8,3,7]))
