2dc2179a5acf8e2b057c0dec691bfe3afaf22762
sidahmed <ahorob57@gmail.com>
2020-12-06 14:22:03 +0100

    add permute-leetcode

--- tests.js
+++ tests.js
@@ -1,36 +1,36 @@
 // write your leetcode tests here
 /*
-  merge two sorted list
-  Input: l1 = [1,2,4], l2 = [1,3,4]
-  Output: [1,1,2,3,4,4]
+  Given an array nums of distinct integers, return all the possible permutations. 
+  You can return the answer in any order.
+
+  Example 1:
+  Input: nums = [1,2,3]
+  Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+
+  Example 2:
+  Input: nums = [0,1]
+  Output: [[0,1],[1,0]]
+
+  Example 3:
+  Input: nums = [1]
+  Output: [[1]]
 */
 
 // your pseudo-code goes here
 /*
 */
 
-
-
-var mergeTwoLists = function(l1, l2) {
-  let dummy = new ListNode()
-  let head = dummy
-
-  while (l1 && l2) {
-    if (l1.val < l2.val) {
-      dummy.next = l1
-      l1 = l1.next
-    } else {
-      dummy.next = l2
-      l2 = l2.next
-    }
-    dummy = dummy.next
-  }
-
-  if (l1) dummy.next = l1
-  if (l2) dummy.next = l2
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(nums) {
+  let perm = []
+   
   
-  return head.next
+  return perm
 }
+
 
 
 console.log(maxArea([1,8,6,2,5,4,8,3,7]))
