c02579db338b55b6bbd07fb79f5ceae5e4a4a863
sidahmed <ahorob57@gmail.com>
2020-12-07 16:05:47 +0100

    minPath solution

--- tests.js
+++ tests.js
@@ -1,56 +1,40 @@
 // write your leetcode tests here
 /*
-  Given an array nums of distinct integers, return all the possible permutations. 
-  You can return the answer in any order.
+  Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right, 
+  which minimizes the sum of all numbers along its path.
+  Note: You can only move either down or right at any point in time.
 
   Example 1:
-  Input: nums = [1,2,3]
-  Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+  Input: grid = [[1,3,1],[1,5,1],[4,2,1]]
+  Output: 7
+  Explanation: Because the path 1 → 3 → 1 → 1 → 1 minimizes the sum.
 
   Example 2:
-  Input: nums = [0,1]
-  Output: [[0,1],[1,0]]
-
-  Example 3:
-  Input: nums = [1]
-  Output: [[1]]
+  Input: grid = [[1,2,3],[4,5,6]]
+  Output: 12
 */
 
 // your pseudo-code goes here
 /*
 */
-
 /**
- * @param {number[]} nums
- * @return {number[][]}
+ * @param {number[][]} grid
+ * @return {number}
  */
-var permute = function(nums) {
-  let perm = []
+var minPathSum = function(grid) {
   
-  const DFS = (nums, path, used, perm) => {
-    if (path.length == nums.length) {
-      // make a deep copy since otherwise we'd be append the same list over and over
-      perm.push(Array.from(path))
-      return
-    }
-
-    for (let i = 0; i < nums.length; i++) {
-      // skip used numbers
-      if (used[i]) continue
-      // add numbers to permutation, mark numbers as used
-      path.push(nums[i])  // path = [1, 2, 3]
-      used[i] = true
-      DFS(nums, path, used, perm)
-      // remove numbers from permutation, mark numbers as unused
-      path.pop()
-      used[i] = false
+  const explore = (grid, shortest=[]) => {
+    let i = 0
+    let j = 0
+    while (i < grid.length) {
+      while (j < grid[i].length) {
+        shortest.push(grid[i][j])
+        j++
+      }
+      i++
     }
   }
   
-  DFS(nums, [], Array.from(nums).fill(false), perm)
-
-  return perm
 }
-
 
 console.log(permute([1,2,3]))
