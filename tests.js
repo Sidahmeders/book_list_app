930118a36e401462beb9bbaed43bd2e6580d6ddb
sidahmed <ahorob57@gmail.com>
2020-12-10 17:33:45 +0100

    n Queens problem leetcode 51

--- tests.js
+++ tests.js
@@ -1,45 +1,31 @@
 // write your leetcode tests here
 /*
-Given an n-ary tree, return the preorder traversal of its nodes' values.
-Nary-Tree input serialization is represented in their level order traversal, 
-each group of children is separated by the null value (See examples).
-
-Follow up:
-Recursive solution is trivial, could you do it iteratively?
+The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that no two queens attack each other.
+Given an integer n, return all distinct solutions to the n-queens puzzle.
+Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' 
+both indicate a queen and an empty space, respectively.
 
 Example 1:
-Input: root = [1,null,3,2,4,null,5,6]
-Output: [1,3,5,6,2,4]
+Input: n = 4
+Output: [[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
+Explanation: There exist two distinct solutions to the 4-queens puzzle as shown above
 
 Example 2:
-Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
-Output: [1,2,3,6,7,11,14,4,8,12,5,9,13,10]
+Input: n = 1
+Output: [["Q"]]
 
 Constraints:
-The height of the n-ary tree is less than or equal to 1000
-The total number of nodes is between [0, 10^4]
-
+1 <= n <= 9
 */
 
 /**
- * @param {Node} root
- * @return {number[]}
+ * @param {number} n
+ * @return {string[][]}
  */
-var preorder = function(root) {
-  let res = []
-  let stack = []
-  if (!root) return res
-  
-  stack.push(root)
-  while (stack.length) {
-      let node = stack.pop()
-      res.push(node.val)
-      for (let i = node.children.length-1; i >= 0; i--) {
-          stack.push(node.children[i])
-      }
-  }
-  
-  return res
+var solveNQueens = function(n) {
+    
 }
 
-console.log(shipWithinDays([1,2,3,4,5,6,7,8,9,10], 5))
+
+
+console.log()
