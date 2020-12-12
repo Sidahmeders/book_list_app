16662171f8702c2f7ecb635fc953a43f9565ba56
sidahmed <ahorob57@gmail.com>
2020-12-12 19:33:33 +0100

    make my owen

--- tests.js
+++ tests.js
@@ -26,27 +26,36 @@ nums[i] will be an integer between 0 and 49,999.
 */
 
 var findShortestSubArray = function(nums) {
-    let degree = 0
-    let repeated = new Set()
-    let subArr = {}
-
-    while (nums.length) {
-        let n = nums[0]
-
-        if (repeated.has(n)) {    
-            degree++
-            subArr[n]++
-        } else {
-            subArr[n] = 1
-            repeated.add(n)
+    let num_counts = new Map();  // hash map to keep count of each number
+    let degree = 0;              // initalize degree to 0
+    
+    let first_seen = new Map();  // hash map keep track of first time we see the number
+    let min_length = 0;
+    
+    for (let i= 0; i<= nums.length; i++){    // loop through the numbers 
+        if(!first_seen.has(nums[i])){   
+            first_seen.set(nums[i], i);  // mark the index of first occurance
         }
-
-        nums = nums.slice(1, nums.length)
+        
+        // key: current number
+        // value: count of number( if nums is in map we get the current count or if not count equals 0 then we increment + 1)
+         num_counts.set(nums[i], (num_counts.has(nums[i]) ? num_counts.get(nums[i]) : 0) + 1)  
+        
+        // if count > degree then we update the degree
+     if(num_counts.get(nums[i]) > degree){
+         degree = num_counts.get(nums[i]);
+         min_length = i - first_seen.get(nums[i]) + 1;     // subtract occurance (max) from first occurance to see the 
+     } else if (num_counts.get(nums[i]) === degree){      // if number is equal to degree
+            min_length = Math.min(min_length, i - first_seen.get(nums[i])+ 1); 
+        }
+        
     }
-
-    return [degree, subArr]
+    return min_length;
 }
 
 
-console.log(findShortestSubArray([1,2,2,3,1])) // degree 2, res 2
-console.log(findShortestSubArray([1,2,2,3,1,4,2])) // degree 3, res 6
+console.log(findShortestSubArray([1,2,2,3,1])) // 2
+console.log(findShortestSubArray([1,2,2,3,1,4,2])) // 6
+console.log(findShortestSubArray([1,1])) // 2
+console.log(findShortestSubArray([1,2,1])) // 3
+console.log(findShortestSubArray([1,1,2,2,2,1])) // 3
